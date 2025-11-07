26d223e87a17ef9a0fefcc71788364375fb94a3e
Anthony Hagidimitriou <info@anthonyhagi.com.au>
2025-11-07 20:38:02 +1030

    feat: add exports check in prs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,6 +4,11 @@ export default defineConfig({
   clean: true,
   dts: true,
   format: ["esm", "cjs"],
+  cjsDefault: false,
+  attw: {
+    level: "error",
+    profile: "node16",
+  },
   platform: "node",
   entry: ["./src/index.ts"],
 });
